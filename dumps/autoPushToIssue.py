8fc6ec46831ff5e19a9853f894246083a5456859
Marcus Vinicius <viniips@hotmail.com>
2020-05-04 17:34:18 -0300

    first commit 20

--- dumps/autoPushToIssue.py
+++
@@ -1,9 +0,0 @@
-import os, random
-
-i = 10
-while i <= 20:
-	os.system('echo 1 >> '+str(random.randrange(2,1000,2))+'.txt')
-	os.system('git add -A')
-	os.system('git commit -m "first commit %s"' % (i))
-	os.system('git push origin master -f')
-	i = i + 1
